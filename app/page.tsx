cb8505e01b899576f5764a0d1a5092376aeab8ca
Kemuel John <kemueljohn73@gmail.com>
2024-12-18 23:14:24 +0800

    "Add favicon to all pages"

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import { prisma } from "@/prisma";
 import NotebooksList from './components/notebooks-list';
+import Head from 'next/head';
 
 export default async function Home() {
     const session = await auth();
@@ -17,21 +18,26 @@ export default async function Home() {
     });
 
     return (
-        <div className='notebook-container'>
-          <div className='welcome-container'>
-            <div className='welcome-left-padding'></div>
-            <div className='welcome-content'>
-              <h1>Welcome, {session.user.name}</h1>
-              <SignOut/>
+        <> 
+          <Head>
+            <link rel="icon" href="/favicon.jpeg" type="image/jpeg" />
+          </Head>
+          <div className='notebook-container'>
+            <div className='welcome-container'>
+              <div className='welcome-left-padding'></div>
+              <div className='welcome-content'>
+                <h1>Welcome, {session.user.name}</h1>
+                <SignOut/>
+              </div>
             </div>
+            <div className='flex flex-row items-center justify-center w-full pt-10'>
+              <h1 className='text-2xl font-bold mb-4'>Your Notebooks:</h1>
+            </div>
+            <Link href="notebooks/new" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded inline-block">
+              Create New Notebook
+            </Link>
+            <NotebooksList notebooks={notebooks} />
           </div>
-          <div className='flex flex-row items-center justify-center w-full pt-10'>
-            <h1 className='text-2xl font-bold mb-4'>Your Notebooks:</h1>
-          </div>
-          <Link href="notebooks/new" className="mt-4 bg-blue-500 text-white py-2 px-4 rounded inline-block">
-            Create New Notebook
-          </Link>
-          <NotebooksList notebooks={notebooks} />
-        </div>
+        </>
     );
 }
